3d62a563cc7e2ae5b5d80dd95c55f6c728634f83
XiZhi <1271910933@qq.com>
2020-06-05 12:02:03 +0800

    重新做的登录界面布局

---
+++ app/src/main/java/cn/edu/scujcc/loginActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.scujcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.edu.scujcc.cityselectdemo.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
