d7063b6a0565b28ced8680dcbee8aa2ab805d6b0
XiZhi <1271910933@qq.com>
2020-06-10 11:10:51 +0800

    完成注册界面布局

--- app/src/main/java/cn/edu/scujcc/registerActivity.java
+++ app/src/main/java/cn/edu/scujcc/registerActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.textfield.TextInputLayout;
+
 import cn.edu.scujcc.weather.R;
 
 public class registerActivity extends AppCompatActivity {
-
+    TextInputLayout birthadayInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //创建一个日历，新建一个Builder
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText("选择生日");
+        MaterialDatePicker picker = builder.build();
+        picker.addOnPositiveButtonClickListener(selection -> {
+           String date = picker.getHeaderText();
+           birthadayInput.getEditText().setText(date);
+        });
+        birthadayInput = findViewById(R.id.r_birthday);
+        birthadayInput.setEndIconOnClickListener(v -> {
+            picker.show(getSupportFragmentManager(),picker.toString());
+        });
     }
 }
